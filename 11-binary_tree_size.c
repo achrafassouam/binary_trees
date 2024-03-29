b522d371d5f2df5e9dee24664e66c76d5be3de9f
“Achraf <achrafassouam@gmail.com>
2024-03-28 19:51:13 -0700

    adding 12th task code, 11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: A pointer to the root node of the tree to measure
+ *
+ * Return: If tree is NULL, return 0.
+ *         Otherwise, return the number of nodes in the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) + 1);
+}
+
