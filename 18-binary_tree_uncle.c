31071a3bd72c241bf47db18f9165c9ec4615aa97
“Achraf <achrafassouam@gmail.com>
2024-03-28 20:33:53 -0700

    adding 19th task code, 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find uncle
+ *
+ * The uncle node is the sibling of the parent node.
+ *
+ * Return: Pointer to uncle node, NULL if node has no uncle or invalid
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent, *grandparent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	grandparent = parent->parent;
+
+	if (grandparent == NULL)
+		return (NULL);
+
+	if (parent == grandparent->left)
+		return (grandparent->right);
+	else
+		return (grandparent->left);
+}
+
